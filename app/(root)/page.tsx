3c9bf183db95303a7ca4cdcbb60b047587df92d5
Deepak Sharma <076bct029@ioepc.edu.np>
2024-08-06 23:08:10 +0545

    implement the Sidebar and DoughnutChart

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,30 @@
+import Header from "@/components/Header";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+
+
 const Home = () => {
+  const loggedIn = { firstName: "Deepak" };
+
   return (
-    <div>Home</div>
-  )
-}
-export default Home
+    <section className="home">
+      <div className="home-content">
+        <div className="home-header">
+          <Header
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={9889}
+          />
+        </div>
+
+      </div>
+    </section>
+  );
+};
+export default Home;
