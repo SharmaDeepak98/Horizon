abdc291c5eaa540eaa8066fede0de3775c44a738
Deepak Sharma <076bct029@ioepc.edu.np>
2024-08-07 22:28:46 +0545

    initialize Right SideBar and add BankCard

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,31 @@
 import Header from "@/components/Header";
+import RightSideBar from "@/components/RightSideBar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
 
 
 const Home = () => {
-  const loggedIn = { firstName: "Deepak" };
+
+  const loggedIn = {
+  
+    $id:"1",
+    firstName:"Deepak",
+    lastName:"Sharma",
+  email: "example@abc.com",
+  userId: "123",
+  dwollaCustomerUrl: "www.example.com",
+  dwollaCustomerId: "abcd",
+  address1: "addressOfUser",
+  city: "myCity",
+  state: "myState",
+  postalCode: "123123",
+  dateOfBirth: "12-12-2001",
+  ssn: "12"
+  }
 
   return (
     <section className="home">
       <div className="home-content">
-        <div className="home-header">
+        <header className="home-header">
           <Header
             type="greeting"
             title="Welcome"
@@ -21,9 +38,14 @@ const Home = () => {
             totalBanks={1}
             totalCurrentBalance={9889}
           />
-        </div>
-
+        </header>
+USER TRANSACTION
       </div>
+    <RightSideBar 
+     user={loggedIn}
+     transactions={[]}
+     banks={[{currentBalance:123},{currentBalance:325}]}
+     />
     </section>
   );
 };
