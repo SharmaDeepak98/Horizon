b234e7c47ef360d9ccd92e0df0d708c8a7a50318
Deepak Sharma <076bct029@ioepc.edu.np>
2024-09-30 21:42:44 +0545

    simple modification

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,18 @@
-const Header = ({type="title", title,user,subtext}:HeaderBoxProps) => {
+const Header = ({ type = "title", title, user, subtext }: HeaderBoxProps) => {
   return (
-  <section className="header-box">
-    <h1 className="header-box-title">
-    {title}
-    {type=="greeting"&&(
-        <span className="text-bankGradient"> {user}</span>
-    )}
-    </h1>
-    <p className="header-box-subtext">{subtext}</p>
-  </section>
-  )
-}
-export default Header
+    <section className="header-box">
+      <h1 className="header-box-title">
+        {title}
+        {type == "greeting" && (
+          <>
+            <span className="text-bankGradient "> {user?.split(" ")[0]}</span> 
+            <span className="font-light"> | </span>
+            <span className="font-light"> {user?.split(" ")[1]}</span>
+          </>
+        )}
+      </h1>
+      <p className="header-box-subtext">{subtext}</p>
+    </section>
+  );
+};
+export default Header;
